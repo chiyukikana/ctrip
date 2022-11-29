0793945d669a0b15304f2e924f97471ee8329fae
chiyukikana <chiyukikana@icloud.com>
2022-11-29 20:03:04 +0800

    feat(RouteProvider): add Route Provider

--- src/providers/app.tsx
+++ src/providers/app.tsx
@@ -1,7 +1,8 @@
-import { ConfigProvider as AntdConfigProvider } from 'antd'
 import React, { PropsWithChildren } from 'react'
-import { BrowserRouter as BrowserRouterProvider } from 'react-router-dom'
+import { ConfigProvider as AntdConfigProvider } from 'antd'
+import { RouterProvider } from 'react-router-dom'
 import { Provider as ReactReduxProvider } from 'react-redux'
+import router from '../routes/router'
 import store from '../redux/store'
 import locale from 'antd/locale/zh_CN'
 import 'dayjs/locale/zh-cn'
@@ -10,7 +11,8 @@ export const AppProvider: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <ReactReduxProvider store={store}>
       <AntdConfigProvider locale={locale}>
-        <BrowserRouterProvider>{children}</BrowserRouterProvider>
+        <RouterProvider router={router} fallbackElement={<h2>Loading...</h2>} />
+        {children}
       </AntdConfigProvider>
     </ReactReduxProvider>
   )
