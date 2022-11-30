889a913325ea739fa118b94cace621e09912d0fb
chiyukikana <chiyukikana@icloud.com>
2022-11-30 15:26:15 +0800

    feat(footer): add footer.

--- src/components/GlobalFooter/GlobalFooter.tsx
+++ src/components/GlobalFooter/GlobalFooter.tsx
@@ -1,12 +1,40 @@
 import React from 'react'
-import { Layout, Typography } from 'antd'
-
-const { Footer: AntdFooter } = Layout
+import { GithubOutlined, BugOutlined } from '@ant-design/icons'
+import { DefaultFooter } from '@ant-design/pro-components'
+import { Tooltip, Typography } from 'antd'
 
 export const GlobalFooter: React.FC = () => {
   return (
-    <AntdFooter style={{ textAlign: 'center' }}>
-      <Typography.Text>Copyright Copyright Copyright</Typography.Text>
-    </AntdFooter>
+    <DefaultFooter
+      copyright="Copyright 1999-2022, ctrip.com. All rights reserved."
+      links={[
+        {
+          key: 'github',
+          title: (
+            <Tooltip title="支持项目">
+              <Typography.Text>
+                <GithubOutlined />
+                <span>支持项目</span>
+              </Typography.Text>
+            </Tooltip>
+          ),
+          href: 'https://github.com/chiyukikana/ctrip',
+          blankTarget: true,
+        },
+        {
+          key: 'issues',
+          title: (
+            <Tooltip title="问题反馈">
+              <Typography.Text>
+                <BugOutlined />
+                <span>问题反馈</span>
+              </Typography.Text>
+            </Tooltip>
+          ),
+          href: 'https://github.com/chiyukikana/ctrip/issues',
+          blankTarget: true,
+        },
+      ]}
+    />
   )
 }
