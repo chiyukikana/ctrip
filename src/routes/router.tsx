e0c93b0e0b56d81900292276b3b61cbed29bf8d8
chiyukikana <chiyukikana@icloud.com>
2022-11-30 10:46:21 +0800

    feat(route): add 404 route.

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,6 +2,13 @@ import { createBrowserRouter } from 'react-router-dom'
 import { protectedRoutes } from './protected'
 import { publicRoutes } from './public'
 
-const router = createBrowserRouter([...publicRoutes, ...protectedRoutes])
+const router = createBrowserRouter([
+  ...publicRoutes,
+  ...protectedRoutes,
+  {
+    path: '*',
+    element: <div>404</div>,
+  },
+])
 
 export default router
