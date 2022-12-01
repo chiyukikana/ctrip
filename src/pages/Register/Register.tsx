09d00bb0aae659bf0497e1ea0b339bec28c2904e
chiyukikana <chiyukikana@icloud.com>
2022-12-01 20:20:12 +0800

    feat(SignUp): add register page.

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,3 +1,4 @@
+import { Button, Col, Input, Row, Space, Typography } from 'antd'
 import React from 'react'
 import { Helmet } from 'react-helmet-async'
 import { UserLayout } from '../../layouts'
@@ -8,6 +9,35 @@ export const Register: React.FC = () => {
       <Helmet>
         <title>用户注册</title>
       </Helmet>
+      <Row style={{ paddingTop: 64 }} gutter={[0, 16]}>
+        <Col span={24}>
+          <Row align="middle" justify="center">
+            <Space>
+              <Col>
+                <Typography.Text strong>邮箱</Typography.Text>
+              </Col>
+              <Col>
+                <Input style={{ width: 200 }} />
+              </Col>
+            </Space>
+          </Row>
+        </Col>
+        <Col span={24}>
+          <Row align="middle" justify="center">
+            <Space>
+              <Col>
+                <Typography.Text strong>密码</Typography.Text>
+              </Col>
+              <Col>
+                <Input.Password style={{ width: 200 }} />
+              </Col>
+            </Space>
+          </Row>
+        </Col>
+        <Col span={24} style={{ textAlign: 'center' }}>
+          <Button type="primary">注册</Button>
+        </Col>
+      </Row>
     </UserLayout>
   )
 }
