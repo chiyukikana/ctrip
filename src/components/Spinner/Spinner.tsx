c366ca1d98a7625efcf4dcc6743e381cdbf432fe
chiyukikana <chiyukikana@icloud.com>
2022-12-01 20:04:54 +0800

    perf(interface): IProps extends PropsWithChildren

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -2,14 +2,14 @@ import React, { PropsWithChildren } from 'react'
 import { Spin } from 'antd'
 import { SpinSize } from 'antd/es/spin'
 
-interface PropsType extends PropsWithChildren {
+interface PropsType {
   loading: boolean
   extraRender?: (spin: React.ReactElement) => React.ReactNode
   style?: React.CSSProperties
   size?: SpinSize
 }
 
-export const Spinner: React.FC<PropsType> = ({
+export const Spinner: React.FC<PropsWithChildren<PropsType>> = ({
   loading,
   extraRender,
   style,
